c4264c2d0563faace6521b8afa46b8945aae35d4
Hugo Moura <hugoeamoura@gmail.com>
2022-06-01 10:47:01 -0300

    addcionado index.html e index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+// Criando mais de uma função.
+// Com os conhecimentos adquiridos na aula de
+// funções crie uma função para cada operação
+// matemática (soma, divisão, multiplicação e
+// subtração).
+// Exemplo:
+// function somar(numero1, numero2) {
+// let soma = numero1 + numero2;
+// return 'A soma dos dois números é: ' + soma;
+// }
+// Realize a chamada de cada função, colocando
+// cada resultado em uma variável e mostre a
+// resposta usando o console.log.
+
+function somar(numero1, numero2) {
+    return numero1 + numero2
+    }
+
+    somar (5, 6) 
+
+    let resultadoSoma = somar (5, 6)
+    console.log('A soma dos números é ' + resultadoSoma)
+
+function subtrair(numero1, numero2) {
+    return numero1 - numero2
+    }
+
+    subtrair (4, 2)
+
+    let resultadoSubtracao = subtrair (4, 2)
+    console.log('A subtração dos números é '+ resultadoSubtracao)
+
+    function multiplicar(numero1, numero2) {
+        return numero1 * numero2
+        }
+    
+        multiplicar (6, 2)
+    
+        let resultadoMultiplicacao = multiplicar (6, 2)
+        console.log('A multiplicação dos números é '+ resultadoMultiplicacao)
+
+       
+ function dividir(numero1, numero2) {
+        return numero1 / numero2
+        }
+        
+        dividir (6, 2)
+        
+        let resultadoDivisao = dividir (6, 2)
+        console.log('A multiplicação dos números é '+ resultadoDivisao)
+    
